63ce6f047a827a7a331709669e1d6e9c82b5eb87
wangdeliang <wangdeliang@fehorizon.com>
2024-06-07 10:32:06 +0800

    #feat: project setup

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -3,5 +3,16 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    console.log("Received request: ", request.title);
+    if (request.title == "bingSearch") {
+        searchRandomWord(request.message);
+    }
 });
+
+function searchRandomWord(randomWord) {
+//    const form = "QBLH";
+    const form2 = "HPNN01";
+    const searchURL = `https://cn.bing.com/search?q=${randomWord}&form=${form2}`;
+    console.log("Random url: ", searchURL);
+    window.location.href = searchURL;
+}
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,14 @@
-console.log("Hello World!", browser);
+import words from './words.js';
+
+function startRandomSearch() {
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        let randomWord = words[Math.floor(Math.random() * words.length)]
+        browser.tabs.sendMessage(activeTab.id, { title: "bingSearch", message: randomWord }, function (res) {
+            
+        });
+    });
+}
+
+startRandomSearch();
+setInterval(startRandomSearch, 6000);
