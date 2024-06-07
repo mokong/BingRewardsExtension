faa75108d7572dfff84f88cf628bc46ae9f9eaac
wangdeliang <wangdeliang@fehorizon.com>
2024-06-07 15:01:18 +0800

    #feat: 添加最大搜索次数

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,6 +1,17 @@
 import words from './words.js';
 
+// 设置最大搜索次数
+const maxSearchCount = 30;
+let searchCount = 0;
+
+
 function startRandomSearch() {
+    searchCount++;
+    if (searchCount >= maxSearchCount) {
+        clearInterval(searchInterval);
+        console.log('已达到最大搜索次数');
+    }
+    
     browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
         let randomWord = words[Math.floor(Math.random() * words.length)]
@@ -10,5 +21,5 @@ function startRandomSearch() {
     });
 }
 
+const searchInterval = setInterval(startRandomSearch, 6000);
 startRandomSearch();
-setInterval(startRandomSearch, 6000);
