b666e4aee26a507bbec6799fb36107a1c79175cd
wangdeliang <wangdeliang@fehorizon.com>
2024-06-28 10:28:16 +0800

    #feat: 图标替换；修改平台判断方式

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -5,7 +5,8 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request.title);
     if (request.title == "bingSearch") {
-        searchRandomWord(request.message, request.form);
+        let form = getURLForm();
+        searchRandomWord(request.message, form);
     }
 });
 
@@ -13,4 +14,29 @@ function searchRandomWord(randomWord, form) {
     const searchURL = `https://cn.bing.com/search?q=${randomWord}&form=${form}&cvid={cvid}`;
     console.log("Random url: ", searchURL);
     window.location.href = searchURL;
+}
+
+function getURLForm() {
+    let os = getOS();
+    if (os === "Mac") {
+        return "QBLH";
+    } else {
+        return "HPNN01";
+    }
+}
+
+function getOS() {
+  const userAgent = window.navigator.userAgent;
+  const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
+//  const iosPlatforms = ['iPhone', 'iPad', 'iPod']; // 添加 iOS 平台
+  let os = 'iPhone';
+
+    for (let i = 0; i < macosPlatforms.length; i++) {
+        if (userAgent.indexOf(macosPlatforms[i]) !== -1) {
+            os = 'Mac';
+            break;
+        }
+    }
+
+  return os;
 }
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -15,39 +15,10 @@ function startRandomSearch() {
     browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
         let randomWord = words[Math.floor(Math.random() * words.length)]
-        let form = getURLForm();
-        
-        console.log('form:', form);
-        
-        browser.tabs.sendMessage(activeTab.id, { title: "bingSearch", message: randomWord, form: form }, function (res) {
+        browser.tabs.sendMessage(activeTab.id, { title: "bingSearch", message: randomWord }, function (res) {
             
         });
     });
-}
-
-function getURLForm() {
-    let os = getOS();
-    if (os === "Mac") {
-        return "QBLH";
-    } else {
-        return "HPNN01";
-    }
-}
-
-function getOS() {
-  const userAgent = window.navigator.userAgent;
-  const platform = window.navigator.platform;
-  const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-  const iosPlatforms = ['iPhone', 'iPad', 'iPod']; // 添加 iOS 平台
-  let os = null;
-
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = 'Mac';
-  } else if (iosPlatforms.indexOf(userAgent) !== -1) { // 修改判断条件为 userAgent
-    os = 'iPhone';
-  }
-
-  return os;
 }
 
 const searchInterval = setInterval(startRandomSearch, 7000);
